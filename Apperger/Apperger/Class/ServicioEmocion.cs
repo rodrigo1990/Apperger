164971de2479cff046210e80f0e16ed5b513a0a9
Maxi <maximilianoariela@gmail.com>
2018-10-15 17:02:58 -0300

    se suben cambios para detectar emociones api microsoft

---
+++ Apperger/Apperger/Class/ServicioEmocion.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Newtonsoft.Json;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Apperger
+{
+    public class ServicioEmocion
+    {
+
+        public static async Task<string> MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+            
+            const string uriBase =
+           "https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/detect";
+
+            const string key = "3a4d2f5b4d71475bbcb8fbc6fcd53a78";
+
+           
+            // Request headers.
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apim-Subscription-Key", key);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+              
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+                return contentString;
+                /*string jsonRes = JsonPrettyPrint(contentString);
+                return jsonRes;*/
+
+
+            }
+
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+
+    }
+
+
+
+}
+
+
--- Apperger/Apperger/SacarUnaFoto.xaml.cs
+++ Apperger/Apperger/SacarUnaFoto.xaml.cs
@@ -5,20 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using Plugin.Media.Abstractions;
 using Plugin.Media;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Common.Contract;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Apperger.Data;
 
 namespace Apperger
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SacarUnaFoto : ContentPage
     {
+        static Stream streamCopy;
+        double felicidad;
+        double enojo;
+        double asco;
+        double miedo;
+        double neutral;
+        double tristeza;
+        double sorpresa;
+        double desprecio;
+        string mensaje;
+
         public SacarUnaFoto()
         {
             InitializeComponent();
+            
         }
 
+        
         private async void HacerFoto(object sender, EventArgs e)
         {
 
@@ -32,7 +52,81 @@ namespace Apperger
 
             var foto = await CrossMedia.Current.TakePhotoAsync(opciones_almacenamiento);
 
+            if (foto == null)
+                return;
 
+            MiImagen.Source = ImageSource.FromStream(() =>
+            {
+                var stream = foto.GetStream();
+                streamCopy = new MemoryStream();
+                stream.CopyTo(streamCopy);
+                stream.Seek(0, SeekOrigin.Begin);
+                foto.Dispose();
+                return stream;
+            });
+            
+            string  json = await ServicioEmocion.MakeAnalysisRequest(foto.AlbumPath);
+           
+            List<Emociones> emociones = JsonConvert.DeserializeObject<List<Emociones>>(json);
+            
+                foreach (var lista in emociones)
+                {
+                    felicidad = lista.faceAttributes.emotion.happiness;
+                    enojo = lista.faceAttributes.emotion.anger;
+                    desprecio = lista.faceAttributes.emotion.contempt;
+                    asco = lista.faceAttributes.emotion.disgust;
+                    miedo = lista.faceAttributes.emotion.fear;
+                    neutral = lista.faceAttributes.emotion.neutral;
+                    tristeza = lista.faceAttributes.emotion.sadness;
+                    sorpresa = lista.faceAttributes.emotion.surprise;
+                }
+
+                if (felicidad > 0.5)
+                {
+                    mensaje = "Muy bien, estas feliz!!";
+                }
+
+                if (enojo > 0.5)
+                {
+                    mensaje = "¿Porque estas enojado?";
+                }
+
+                if (desprecio > 0.5)
+                {
+                    mensaje = "No se te ve muy contento!!";
+                }
+
+                if (asco > 0.5)
+                {
+                    mensaje = "¿No te gusta algo?";
+                }
+
+                if (miedo > 0.5)
+                {
+                    mensaje = "¿Que te asusta?";
+                }
+
+                if (neutral > 0.5)
+                {
+                    mensaje = "Estas Normal";
+                }
+
+                if (tristeza > 0.5)
+                {
+                    mensaje = "¿Porque estas triste?";
+                }
+
+                if (sorpresa > 0.5)
+                {
+                    mensaje = "Te veo muy soprendido!";
+                }
+
+               
+                await DisplayAlert("Msj", mensaje, "Ok");
+            
+                
+
+           
         }//hacerfoto
 
         private async void ElegirFoto(Object sender, EventArgs e)
@@ -53,6 +147,6 @@ namespace Apperger
             }
         }
 
-
+        
     }
 }
